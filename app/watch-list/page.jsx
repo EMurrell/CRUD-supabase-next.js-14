aabde2523aa1044660de7510e50e1d4523464e4c
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-04-23 14:41:34 -0400

    cleanup

--- app/watch-list/page.jsx
+++ app/watch-list/page.jsx
@@ -4,35 +4,6 @@ import EditWatch from "../components/EditWatch";
 export default function WatchList() {
   const watches = [];
   return (
-    // <div>
-    //   <div>
-    //     <div>
-    //       <h1>my watch list</h1>
-    //       <form action="/auth/signout" method="post">
-    //         <button type="submit" className="p-3 bg-white/20">
-    //           Sign out
-    //         </button>
-    //       </form>
-    //     </div>
-    //     <WatchForm />
-    //     <div>
-    //       {watches.map((watch) => (
-    //         <div key={watch.id}>
-    //           <h2>
-    //             {watch.brand} - {watch.name}
-    //           </h2>
-    //           <div>
-    //             <form action="deleteWatch">
-    //               <input type="hidden" name="id" value={watch.id} />
-    //               <button type="submit">Delete</button>
-    //             </form>
-    //             <EditWatch watch={watch} />
-    //           </div>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
     <div className="min-h-screen text-gray-300 bg-gray-900">
       <div className="container p-6 mx-auto sm:p-12">
         <div className="flex items-start justify-between">
