a56fd0835eb53794273811e9f0270588567821b6
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-04-29 12:36:27 -0400

    add margins, clean up

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -1,10 +1,7 @@
 "use client";
 import { Auth } from "@supabase/auth-ui-react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import {
-  // Import predefined theme
-  ThemeSupa,
-} from "@supabase/auth-ui-shared";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
 export default function AuthForm() {
   const supabase = createClientComponentClient();
@@ -12,35 +9,26 @@ export default function AuthForm() {
     <Auth
       supabaseClient={supabase}
       view="magic_link"
-      showLinks={false}
+      showLinks={true}
       providers={["google", "facebook", "twitter"]}
       redirectTo="https://crud-supabase-nextjs14.vercel.app/auth/callback"
-      // appearance={{
-      //   theme: ThemeSupa,
-      //   variables: {
-      //     default: {
-      //       colors: {
-      //         brand: "red",
-      //         brandAccent: "darkred",
-      //       },
-      //     },
-      //   },
-      // }}
       appearance={{
         style: {
           button: {
             background: "#3f3f46",
             color: "white",
             borderRadius: "12px",
-            padding: "8px 16px",
+            padding: "12px 16px",
+            margin: "6px 0",
             border: "none",
           },
           anchor: { color: "blue" },
           input: {
             background: "#3f3f46",
             borderRadius: "12px",
-            padding: "8px 16px", // Add padding to the button
-            border: "none", // Remove border
+            padding: "12px 16px",
+            margin: "6px 0",
+            border: "none",
           },
         },
       }}
