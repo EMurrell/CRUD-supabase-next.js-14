7fec0d92cdbd85b780c4fc6c8439f131c5bae97b
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-04-29 12:20:17 -0400

    update page bg color

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full h-full max-w-md px-6 py-8 bg-gray-500 bg-opacity-50 rounded-lg bg-clip-padding backdrop-filter backdrop-blur saturate-100 backdrop-contrast-100">
+    <div className="flex items-center justify-center min-h-screen bg-zinc-900">
+      <div className="w-full h-full max-w-md px-6 py-8 bg-opacity-50 rounded-lg bg-zinc-700 backdrop-filter backdrop-blur saturate-100 backdrop-contrast-100">
         <h1 className="mb-4 text-3xl font-semibold text-white">
           Welcome to my CRUD app
         </h1>
