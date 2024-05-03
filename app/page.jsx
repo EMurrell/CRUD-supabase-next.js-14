be3b32f84b77c2aeb17548f16f632f045b97a027
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-05-03 11:22:36 -0400

    update sign in message

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,12 @@ import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen ">
-      <div className="w-full h-full max-w-md p-8 rounded-lg shadow-lg shadow-black/70 bg-white/10 backdrop-blur-md">
+      <div className="w-full h-full max-w-md p-8 m-2 rounded-lg shadow-lg shadow-black/70 bg-white/10 backdrop-blur-md">
         <h1 className="mb-4 text-3xl font-semibold text-white">
-          Welcome to my CRUD app
+          CRUD with Next.js and Supabase
         </h1>
-        <p className="mb-6 text-white">Sign in to get CRUD'n</p>
-        <div className="m-2">
-          <AuthForm />
-        </div>
+        <p className="mb-6 text-white">Please select a sign in method:</p>
+        <AuthForm />
       </div>
     </div>
   );
