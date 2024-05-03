e047bb0a0159aec6ea3fd6dc30d233b55ea54020
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-05-03 11:23:22 -0400

    update padding

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import AuthForm from "./components/AuthForm";
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen ">
-      <div className="w-full h-full max-w-md p-8 m-2 rounded-lg shadow-lg shadow-black/70 bg-white/10 backdrop-blur-md">
+      <div className="w-full h-full max-w-md px-4 py-8 m-2 rounded-lg shadow-lg sm:p-8 shadow-black/70 bg-white/10 backdrop-blur-md">
         <h1 className="mb-4 text-3xl font-semibold text-white">
           CRUD with Next.js and Supabase
         </h1>
