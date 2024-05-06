fad19c83a3184c9bcd4d2a340edc872ebf9c961e
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-05-06 11:05:00 -0400

    add button variants

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,8 +1,20 @@
-export default function Button() {
+import clsx from "clsx";
+
+export default function Button({ type, text, style }) {
+  const buttonClasses = clsx(
+    "px-4 py-2 mt-4 transition duration-200 ease-in-out rounded-xl",
+    {
+      "bg-sky-100 text-sky-700 hover:bg-sky-200 hover:text-sky-950":
+        style === "primary",
+      "bg-zinc-200 text-zinc-700 hover:bg-zinc-300 hover:text-zinc-800":
+        style === "secondary",
+      "bg-red-200 text-red-700 hover:bg-red-300 hover:text-red-800":
+        style === "delete",
+    }
+  );
+
   return (
-    <button
-      type={type}
-      className="px-4 py-2 mt-4 transition duration-200 ease-in-out bg-sky-100 rounded-xl hover:shadow-md hover:shadow-black/95 hover:bg-sky-200 text-sky-700 hover:text-sky-950 sm:mt-0">
+    <button type={type} className={buttonClasses}>
       {text}
     </button>
   );
