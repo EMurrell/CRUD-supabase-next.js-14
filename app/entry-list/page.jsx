c3b1e86e32862d56911729270a740631b58c568e
Eric Murrell <82235988+EMurrell@users.noreply.github.com>
2024-05-08 10:37:31 -0400

    update h1 font weight

--- app/entry-list/page.jsx
+++ app/entry-list/page.jsx
@@ -27,7 +27,9 @@ export default async function EntryList() {
     <div className="relative min-h-screen py-12">
       <div className="container max-w-4xl p-6 mx-auto sm:p-12">
         <div className="flex items-end justify-between">
-          <h1 className="text-5xl text-white md:text-6xl">Entries</h1>
+          <h1 className="text-5xl font-medium text-white md:text-6xl">
+            Entries
+          </h1>
           <form action="/auth/signout" method="post" className="pb-1 pl-1">
             <Button type="submit" text="sign out" style="primary" />
           </form>
